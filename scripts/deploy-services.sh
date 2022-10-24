8378f2d5e61a2b801e0f72ed442d2702278c3c3b
Stefan Boos <kontakt@boos.systems>
2022-10-24 17:35:48 +0200

    feat: set instance size to 16gb, 4vcpu

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -11,5 +11,5 @@ read_env
 
 for INDEX in $(seq 1 ${NUM_INSTANCES}); do
     SERVICE_NAME="${IMAGE}-${INDEX}"
-    gcloud run deploy "${SERVICE_NAME}" --project yascr-365610 --image "europe-west1-docker.pkg.dev/yascr-365610/docker-repository/${IMAGE}" --port 8887 --memory 8G --cpu 2 --min-instances default --max-instances 1 --region europe-west1 --allow-unauthenticated
+    gcloud run deploy "${SERVICE_NAME}" --project yascr-365610 --image "europe-west1-docker.pkg.dev/yascr-365610/docker-repository/${IMAGE}" --port 8887 --memory 16G --cpu 4 --min-instances default --max-instances 1 --region europe-west1 --allow-unauthenticated
 done
